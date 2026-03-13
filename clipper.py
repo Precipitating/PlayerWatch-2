93d6acf8cc83c697756c0b740c08dd7bf89a861f
Precipitating <92101891+Precipitating@users.noreply.github.com>
2026-03-13 21:21:25 +0000

    + Comments

--- clipper.py
+++ clipper.py
@@ -5,7 +5,17 @@ from player import Player
 from database import VIDEO_TRANSITIONS, VIDEO_CONFIG
 
 
-def get_video_size(path):
+def get_video_size(path: str):
+    """
+    Get the size of the provided video file via path.
+    Used for scaling the watermark consistently.
+
+    Args:
+        path (str): Path to the video file.
+
+    Returns:
+        Width and Height of the video file.
+    """
     result = subprocess.run([
         "ffprobe", "-v", "quiet",
         "-print_format", "json",
@@ -27,6 +37,11 @@ class Clipper:
         self.has_transitions = False
 
     def add_offsets_to_segments(self, player_events):
+        """
+        Apply offsets to the start of the timeframe for the chosen player events list.
+        Args:
+            player_events (list) : List of events for chosen player
+        """
         for event in player_events:
             start = max(0, event["start"] - VIDEO_CONFIG["start_offset"])
             end = event["end"]
@@ -34,6 +49,9 @@ class Clipper:
         self.merge_segments()
 
     def merge_segments(self):
+        """
+        Resolve overlapping timestamps in self.segments via merge
+        """
         sorted_segs = sorted(self.segments, key=lambda x: x[0])
         merged = [sorted_segs[0]]
         for start, end in sorted_segs[1:]:
@@ -46,11 +64,21 @@ class Clipper:
 
     # 1. Trim
     def trim_clips(self):
+        """
+        Start applying ffmpeg trim commands using self.segments (player action timestamp)
+        At this stage, all timestamp offsets and merges should be done by now, and this is
+        the FINAL timestamp.
+        """
         for i, (start, end) in enumerate(self.segments):
             self.parts.append(f"[0:v]trim={start}:{end},setpts=PTS-STARTPTS[v{i}]")
 
     # 2. Transitions
     def apply_transitions(self, player: Player):
+        """
+        Apply transitions using xFade per player action.
+        Args:
+            player (Player): The player clas, used in this function to get user input for chosen transition
+        """
         n = len(self.segments)
         td = VIDEO_CONFIG["transition_time"]
 
@@ -93,6 +121,12 @@ class Clipper:
 
     # 3. Watermark filter
     def define_watermark(self, video_path: str):
+        """
+        Define a watermark image via ffmpeg command on every video clip, on the bottom center with padding.
+        This also ensures a consistent scaling, ignoring the dimension size of the image.
+        Args:
+            video_path (str): The video path, to apply the watermark to.
+        """
         watermark_path = VIDEO_CONFIG["watermark_path"]
         if watermark_path:
             vid_w, vid_h = get_video_size(video_path)
@@ -107,7 +141,13 @@ class Clipper:
             self.video_out = "[wmv]"
 
     # 4. Audio
-    def apply_custom_audio(self, audio_path, video_path):
+    def apply_custom_audio(self, audio_path: str, video_path: str):
+        """
+        Apply custom, looping audio to each clip.
+        Args:
+            audio_path (str): The custom audio path.
+            video_path (str): The video path to apply custom audio to
+        """
         n = len(self.segments)
 
         if audio_path:
@@ -138,6 +178,9 @@ class Clipper:
 
     # 5. Watermark input
     def apply_watermark(self):
+        """
+        Apply the watermark image command
+        """
         watermark_path = VIDEO_CONFIG["watermark_path"]
         if watermark_path:
             wm_index = len([x for x in self.inputs if x == "-i"])
@@ -146,6 +189,10 @@ class Clipper:
 
     # 6. Build
     def build_final_cmd(self, output_file: str):
+        """
+        Return the final ffmpeg command, with all options correctly added.
+        Args: output_file (str): The output path for the video to be saved in.
+        """
         return [
             "ffmpeg", "-y", *self.inputs,
             "-filter_complex", ";".join(self.parts),
